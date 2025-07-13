69a85cf195e2c44125b5f2fa88c6ee0f958bd900
ravimani1001 <inforavimani@gmail.com>
2025-07-13 21:30:34 +0530

    Create venue model

---
+++ backend/models/Venue.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const venueSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  capacity: {
+    type: Number,
+    required: true,
+  },
+
+  images: [
+    {
+      type: String,
+    }
+  ],
+
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+
+  bookedDates: [
+    {
+      type: Date,
+    }
+  ]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Venue', venueSchema);
