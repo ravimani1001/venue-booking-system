37803f20b6fb0eb6b6ead6b97cbb1edaaad6304a
ravimani1001 <inforavimani@gmail.com>
2025-07-18 23:54:20 +0530

    container and py-8 removed from mainlayout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen font-sans text-darkText">
       <Header />
-      <main className="flex-1 container py-8">
+      <main className="flex-1">
         {children}
       </main>
       <Footer />
